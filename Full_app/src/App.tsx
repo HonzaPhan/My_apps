66ae90b2d2ad44f5ea01d478714470f80139bd8c
Long Phan <115040888+HonzaPhan@users.noreply.github.com>
2023-10-02 22:23:08 +0200

    add: todo list

--- Full_app/src/App.tsx
+++ Full_app/src/App.tsx
@@ -2,31 +2,29 @@ import { Provider } from "react-redux";
 import Layout from "./Layout";
 import { store } from "./state/store";
 import { useState } from "react";
-import { WeatherDataContext } from "./state/context/weatherDataContext";
-import { Button } from "@mui/material";
+import { Button, ThemeProvider, CssBaseline } from "@mui/material";
 import { DarkTheme, LightTheme } from "./Theme/Theme";
-import { ThemeContext, ThemeMode } from "./state/context/themeContext";
 
 function App() {
-  const [testState, setTestState] = useState<string>("test");
-  const [themeState, setThemeState] = useState<ThemeMode>(LightTheme);
-
+  const [themeState, setThemeState] = useState(LightTheme);
+∂
   const toggleTheme = () => {
     setThemeState(themeState === LightTheme ? DarkTheme : LightTheme);
   };
-  
+
   return (
     <Provider store={store}>
-      <ThemeContext.Provider value={{ theme: themeState, setTheme: setThemeState }}>
-        <WeatherDataContext.Provider
-          value={{ value: testState, setValue: setTestState }}
+      <ThemeProvider theme={themeState}>
+        <CssBaseline />
+        <Layout />
+        <Button
+          onClick={toggleTheme}
+          sx={{ position: "absolute", bottom: 15, left: 15 }}
+          variant="contained"
         >
-          <Layout />
-        </WeatherDataContext.Provider>
-        <Button onClick={toggleTheme} sx={{ position: "absolute", bottom: 15, left: 15 }}>
           THEME
         </Button>
-      </ThemeContext.Provider>
+      </ThemeProvider>
     </Provider>
   );
 }
